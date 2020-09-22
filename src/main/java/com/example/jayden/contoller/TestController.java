c9d779298e224924940e4847ebbce28d4befb7f2
fadu-jayden <cogito1016@fadutec.com>
2020-09-22 17:55:00 +0900

    파일업로드는 백엔드 멀티파트로 처리

--- src/main/java/com/example/jayden/contoller/TestController.java
+++ src/main/java/com/example/jayden/contoller/TestController.java
@@ -2,15 +2,17 @@ package com.example.jayden.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Controller
+@RequestMapping("/api")
 public class TestController {
 
-    @GetMapping(path = "/hello")
+    @GetMapping("/test")
     @ResponseBody
     public String hello() {
         return LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
