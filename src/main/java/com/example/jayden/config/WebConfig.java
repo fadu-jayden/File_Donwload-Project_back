e2ba56ea693589a647b88c5bafd5f6c4d90c3d79
fadu-jayden <cogito1016@fadutec.com>
2020-10-06 14:30:35 +0900

    서버배포에 맞게 코드수정

--- src/main/java/com/example/jayden/config/WebConfig.java
+++ src/main/java/com/example/jayden/config/WebConfig.java
@@ -1,6 +1,4 @@
 package com.example.jayden.config;
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,8 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
      *
      * ※크로스 도메인 이슈: 브라우저에서 다른 도메인으로 URL 요청을 하는 경우 나타나는 보안문제
      */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowCredentials(true);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//
+//        registry.addMapping("/api/**").allowCredentials(true);
+//    }
 }
--- src/main/java/com/example/jayden/contoller/TestController.java
+++ src/main/java/com/example/jayden/contoller/TestController.java
@@ -23,7 +23,7 @@ public class TestController {
         System.out.println(mFile.getOriginalFilename());
         try{
 //            mFile.transferTo(new File("D:/workspace/annes_order/filedownload_project/public/datas/"+mFile.getOriginalFilename()));
-            mFile.transferTo(new File("C:/Program Files/Apache Software Foundation/Tomcat 8.5/webapps/ROOT/datas/"+mFile.getOriginalFilename()));
+            mFile.transferTo(new File("C:/nginx-1.18.0/html/datas/"+mFile.getOriginalFilename()));
         }catch (IllegalStateException | IOException e) {
             e.printStackTrace();
             return "0";
@@ -34,7 +34,7 @@ public class TestController {
     @GetMapping("/getFiles")
     @ResponseBody
     public List<FileDTO> fileList(){
-        File dir = new File("C:/Program Files/Apache Software Foundation/Tomcat 8.5/webapps/ROOT/datas/");
+        File dir = new File("C:/nginx-1.18.0/html/datas/");
         File files[] = dir.listFiles();
 //        System.out.println("리스트 접근은 했다.");
 
