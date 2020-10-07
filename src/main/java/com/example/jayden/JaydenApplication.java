516454f0e1ff8b8a8ed762cac5230e00edb56031
fadu-jayden <cogito1016@fadutec.com>
2020-10-07 19:58:40 +0900

    한글깨재ㅣㅁ

--- src/main/java/com/example/jayden/JaydenApplication.java
+++ src/main/java/com/example/jayden/JaydenApplication.java
@@ -1,11 +1,15 @@
 package com.example.jayden;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.ServletContextApplicationContextInitializer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class JaydenApplication extends SpringBootServletInitializer {
 
