9fbbb543d8ba66c9ae364457015b7475db392dc7
fadu-jayden <cogito1016@fadutec.com>
2020-10-07 17:01:48 +0900

    다른 다운로드 소스를 긁어왔다

--- src/main/java/com/example/jayden/contoller/TestController.java
+++ src/main/java/com/example/jayden/contoller/TestController.java
@@ -1,6 +1,11 @@
 package com.example.jayden.contoller;
 
 import com.example.jayden.dto.FileDTO;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -55,44 +60,56 @@ public class TestController {
     }//fileList() end
 
     @PostMapping(path = "downloadFile")
-    public void fileDownload(HttpServletResponse response, @RequestParam("checkedFiles") List<String> checkedFiles) throws IOException {
+    public List<ResponseEntity<Resource>> fileDownload(HttpServletResponse response, @RequestParam("checkedFiles") List<String> checkedFiles) throws IOException {
         System.out.println("다운로드 접근");
+        List<ResponseEntity<Resource>> list = new ArrayList<>();
+        
         for(String fileName : checkedFiles){
-            System.out.println(fileName+" 을 다운로드합니다");
-            String saveFileName = "/data/work/servers/tomcat9_ae_fileIO_back/datas/"+fileName;
+//            System.out.println(fileName+" 을 다운로드합니다");
+//            String saveFileName = "/data/work/servers/tomcat9_ae_fileIO_back/datas/"+fileName;
+//
+//            Path source = Paths.get(saveFileName);
+//            String contentType = Files.probeContentType(source);
+//            System.out.println(contentType+" 의 타입을 가지네요");
+//
+//            File file = new File(saveFileName);
+//            long fileLength = file.length();
+//
+//            response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\";");
+//            response.setHeader("Content-Transfer-Encoding", "binary");
+//            response.setHeader("Content-Type", contentType);
+//            response.setHeader("Content-Length", "" + fileLength);
+//            response.setHeader("Pragma", "no-cache;");
+//            response.setHeader("Expires", "-1;");
+//            // 그 정보들을 가지고 reponse의 Header에 세팅한 후
+//
+//
+//            try{
+//                FileInputStream fis = new FileInputStream(saveFileName);
+//                OutputStream out = response.getOutputStream();
+//                // saveFileName을 파라미터로 넣어 inputStream 객체를 만들고
+//                // response에서 파일을 내보낼 OutputStream을 가져와서
+//                int readCount = 0;
+//                byte[] buffer = new byte[1024];
+//                // 파일 읽을 만큼 크기의 buffer를 생성한 후
+//                while ((readCount = fis.read(buffer)) != -1) {
+//                    out.write(buffer, 0, readCount);
+//                    // outputStream에 씌워준다
+//                }
+//            } catch (Exception ex) {
+//                throw new RuntimeException("file Load Error");
+//            }
 
-            Path source = Paths.get(saveFileName);
-            String contentType = Files.probeContentType(source);
-            System.out.println(contentType+" 의 타입을 가지네요");
+            Path path = Paths.get("/data/work/servers/tomcat9_ae_fileIO_back/datas/"+fileName);
+            String contentType = Files.probeContentType(path);
 
-            File file = new File(saveFileName);
-            long fileLength = file.length();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + path.getFileName().toString());
 
-            response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\";");
-            response.setHeader("Content-Transfer-Encoding", "binary");
-            response.setHeader("Content-Type", contentType);
-            response.setHeader("Content-Length", "" + fileLength);
-            response.setHeader("Pragma", "no-cache;");
-            response.setHeader("Expires", "-1;");
-            // 그 정보들을 가지고 reponse의 Header에 세팅한 후
-
-
-            try{
-                FileInputStream fis = new FileInputStream(saveFileName);
-                OutputStream out = response.getOutputStream();
-                // saveFileName을 파라미터로 넣어 inputStream 객체를 만들고
-                // response에서 파일을 내보낼 OutputStream을 가져와서
-                int readCount = 0;
-                byte[] buffer = new byte[1024];
-                // 파일 읽을 만큼 크기의 buffer를 생성한 후
-                while ((readCount = fis.read(buffer)) != -1) {
-                    out.write(buffer, 0, readCount);
-                    // outputStream에 씌워준다
-                }
-            } catch (Exception ex) {
-                throw new RuntimeException("file Load Error");
-            }
-
+            Resource resource = new InputStreamResource(Files.newInputStream(path));
+            list.add(new ResponseEntity<>(resource, headers, HttpStatus.OK));
         }
+        return list;
     }//fileDownload() end
 }
